03939d9d648a6b3aba3e86f65b1c8f78f33c357c
Aaron C Payne <aaron.payne0211@gmail.com>
2025-09-21 17:00:38 -0400

    AI-104: tidy Meteostat client imports

--- src/nfl_pred/weather/__init__.py
+++ src/nfl_pred/weather/__init__.py
@@ -1,5 +1,13 @@
 """Weather-related utilities and clients."""
 
+from .meteostat_client import MeteostatClient, MeteostatClientError, StationRecord
 from .nws_client import NWSClient, gridpoint_forecast, point_metadata
 
-__all__ = ["NWSClient", "gridpoint_forecast", "point_metadata"]
+__all__ = [
+    "MeteostatClient",
+    "MeteostatClientError",
+    "NWSClient",
+    "StationRecord",
+    "gridpoint_forecast",
+    "point_metadata",
+]
