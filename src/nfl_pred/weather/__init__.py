2e4fac2ebb6097c4ee1cca2ebc9572db98d40b31
Aaron C Payne <aaron.payne0211@gmail.com>
2025-09-21 17:28:10 -0400

    AI-106: implement weather artifact storage

--- src/nfl_pred/weather/__init__.py
+++ src/nfl_pred/weather/__init__.py
@@ -2,6 +2,12 @@
 
 from .meteostat_client import MeteostatClient, MeteostatClientError, StationRecord
 from .nws_client import NWSClient, gridpoint_forecast, point_metadata
+from .storage import (
+    WeatherArtifact,
+    WeatherArtifactMetadata,
+    WeatherArtifactStore,
+    build_artifact_key,
+)
 
 __all__ = [
     "MeteostatClient",
@@ -10,4 +16,8 @@ __all__ = [
     "StationRecord",
     "gridpoint_forecast",
     "point_metadata",
+    "WeatherArtifact",
+    "WeatherArtifactMetadata",
+    "WeatherArtifactStore",
+    "build_artifact_key",
 ]
